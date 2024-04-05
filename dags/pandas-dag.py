3920cbe2035eead1fa332980cf602de46aac399b
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-04-05 11:32:44 +0100

    Update pandas-dag.py

--- dags/pandas-dag.py
+++ dags/pandas-dag.py
@@ -2,7 +2,7 @@
 
 from airflow import DAG
 from datetime import datetime
-from pandas import Series
+from pandas import Series, DataFrame
 from airflow.operators.python import PythonOperator
 
 # Configure DAG
@@ -23,6 +23,19 @@ with DAG('pandas_twice_dag', default_args=default_args, schedule_interval=None) 
     # Define task to create Series
     create_series = PythonOperator(
         task_id='create_series',
+        python_callable=create_series_task,
+    )
+    def second_task():
+        # Creating a dictionary with data
+        data2 = {'Name': ['John', 'Anna', 'Peter'], 'Age': [28, 24, 22]}
+        # Converting the dictionary into a DataFrame
+        df = pandas.DataFrame(data2)
+        # Printing the DataFrame
+        print(df)
+
+    # Define
+    create_second = PythonOperator(
+        task_id='create_second',
         python_callable=create_series_task,
     )
 
