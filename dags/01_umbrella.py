465cd4eeaea6a572bf8dde42feb544255a283a9b
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-04-12 10:15:15 +0100

    Update 01_umbrella.py

--- dags/01_umbrella.py
+++ dags/01_umbrella.py
@@ -1,7 +1,6 @@
 import airflow.utils.dates
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from airflow.providers.cncf.kubernetes.operators.pod import KubernetesJobOperator
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
 
 dag = DAG(
     dag_id="01_umbrella",
@@ -10,25 +9,17 @@ dag = DAG(
     schedule_interval="@daily",
 )
 
-k8s_job = KubernetesJobOperator(
-    task_id="job-task",
-    namespace="default",
-    image="perl:5.34.0",
-    cmds=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"],
-    name="test-pi",
+
+
+k = KubernetesPodOperator(
+    name="hello-dry-run",
+    image="debian",
+    cmds=["bash", "-cx"],
+    arguments=["echo", "10"],
+    labels={"foo": "bar"},
+    task_id="dry_run_demo",
+    do_xcom_push=True,
 )
 
-fetch_weather_forecast = DummyOperator(task_id="fetch_weather_forecast", dag=dag)
-fetch_sales_data = DummyOperator(task_id="fetch_sales_data", dag=dag)
-clean_forecast_data = DummyOperator(task_id="clean_forecast_data", dag=dag)
-clean_sales_data = DummyOperator(task_id="clean_sales_data", dag=dag)
-join_datasets = DummyOperator(task_id="join_datasets", dag=dag)
-train_ml_model = DummyOperator(task_id="train_ml_model", dag=dag)
-deploy_ml_model = DummyOperator(task_id="deploy_ml_model", dag=dag)
 
-# Set dependencies between all tasks
-
-job-task >> fetch_weather_forecast >> clean_forecast_data
-fetch_sales_data >> clean_sales_data
-[clean_forecast_data, clean_sales_data] >> join_datasets
-join_datasets >> train_ml_model >> deploy_ml_model
+k.dry_run()
