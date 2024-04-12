170f32d26b6d57652c5bb90c11368a611ae5b783
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-04-12 10:08:36 +0100

    Update 01_umbrella.py

--- dags/01_umbrella.py
+++ dags/01_umbrella.py
@@ -9,6 +9,14 @@ dag = DAG(
     schedule_interval="@daily",
 )
 
+k8s_job = KubernetesJobOperator(
+    task_id="job-task",
+    namespace="default",
+    image="perl:5.34.0",
+    cmds=["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"],
+    name="test-pi",
+)
+
 fetch_weather_forecast = DummyOperator(task_id="fetch_weather_forecast", dag=dag)
 fetch_sales_data = DummyOperator(task_id="fetch_sales_data", dag=dag)
 clean_forecast_data = DummyOperator(task_id="clean_forecast_data", dag=dag)
@@ -18,7 +26,8 @@ train_ml_model = DummyOperator(task_id="train_ml_model", dag=dag)
 deploy_ml_model = DummyOperator(task_id="deploy_ml_model", dag=dag)
 
 # Set dependencies between all tasks
-fetch_weather_forecast >> clean_forecast_data
+
+job-task >> fetch_weather_forecast >> clean_forecast_data
 fetch_sales_data >> clean_sales_data
 [clean_forecast_data, clean_sales_data] >> join_datasets
 join_datasets >> train_ml_model >> deploy_ml_model
