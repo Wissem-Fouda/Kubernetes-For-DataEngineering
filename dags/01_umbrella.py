ff726fc47c5e6339afb8d708eb573601ab50d31a
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-04-15 09:04:05 +0100

    Update 01_umbrella.py

--- dags/01_umbrella.py
+++ dags/01_umbrella.py
@@ -1,25 +1,33 @@
-import airflow.utils.dates
+from datetime import datetime
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
+# Define your default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 4, 15),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1
+}
+
+# Define your DAG
 dag = DAG(
-    dag_id="01_umbrella",
-    description="Umbrella example with DummyOperators.",
-    start_date=airflow.utils.dates.days_ago(5),
-    schedule_interval="@daily",
+    'example_kubernetes_pod',
+    default_args=default_args,
+    description='A simple DAG to demonstrate KubernetesPodOperator',
+    schedule_interval=None
 )
 
-
-
-k = KubernetesPodOperator(
-    name="hello-dry-run",
-    image="debian",
-    cmds=["bash", "-cx"],
-    arguments=["echo", "10"],
-    labels={"foo": "bar"},
-    task_id="dry_run_demo",
-    do_xcom_push=True,
+# Define the task using KubernetesPodOperator
+task1 = KubernetesPodOperator(
+    namespace='default',
+    image="ubuntu:latest",
+    cmds=["echo"],
+    arguments=["Hello, Airflow!"],
+    name="task-1",
+    task_id="task-1",
+    get_logs=True,
+    dag=dag
 )
-
-
-k.dry_run()
