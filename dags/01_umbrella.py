fafb255edd306884aed1ceb28b5fdd7b8c89fb0e
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-04-16 14:43:24 +0100

    Update 01_umbrella.py

--- dags/01_umbrella.py
+++ dags/01_umbrella.py
@@ -36,7 +36,7 @@ task1 = KubernetesPodOperator(
 
 task2 = KubernetesPodOperator(
     namespace='airflow',
-    image="opentest4.azurecr.io/airflow_project/script-code-1-image:latest",
+    image="foudazdocker/demorepo:1.0",
     name="task-2",
     task_id="task-2",
     get_logs=True,
