eef0cb5c15ef2e3921738a90b6af50a083451702
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-05-28 21:47:05 +0100

    Update 01_umbrella.py

--- dags/01_umbrella.py
+++ dags/01_umbrella.py
@@ -1,29 +1,23 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
-
-
-
-
-# Define your default arguments for the DAG
+# Define default arguments for the DAG
 default_args = {
-    'owner': 'airflow',
+    'owner': 'Wissem Fouda',
     'depends_on_past': False,
     'start_date': datetime(2024, 4, 15),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 0
 }
-
-# Define your DAG
+# Define the DAG
 dag = DAG(
     'example_kubernetes_pod',
     default_args=default_args,
     description='A simple DAG to demonstrate KubernetesPodOperator',
     schedule_interval=None
 )
-
-# Define the task using KubernetesPodOperator
+# Define task1 using KubernetesPodOperator
 task1 = KubernetesPodOperator(
     namespace='airflow',
     image="foudazdocker/tasks:1.0",
@@ -32,18 +26,14 @@ task1 = KubernetesPodOperator(
     get_logs=True,
     dag=dag
 )
-
-
+# Define task2 using KubernetesPodOperator
 task2 = KubernetesPodOperator(
     namespace='airflow',
     image="foudazdocker/demorepo:1.0",
     name="task-2",
     task_id="task-2",
     get_logs=True,
-    #volumes=[{"name": "persistent-storage", "persistentVolumeClaim": {"claimName": "pvc-tasks"}}],
-    #volume_mounts=[{"mountPath": "/mnt/persistent_volume", "name": "persistent-storage"}],
     dag=dag
 )
-
 # Set task dependencies
 task1 >> task2
