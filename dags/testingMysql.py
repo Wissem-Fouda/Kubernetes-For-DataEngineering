d73db39172424ba3ed3d64a89874c8a612a2cbfd
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-07-08 10:21:32 +0100

    Update testingMysql.py

--- dags/testingMysql.py
+++ dags/testingMysql.py
@@ -26,7 +26,7 @@ with DAG(
 
     create_table_mssql_task = SQLExecuteQueryOperator(
         task_id="create_country_table",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql=r"""
         CREATE TABLE Country (
             country_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
@@ -54,13 +54,13 @@ with DAG(
     # Example of creating a task that calls an sql command from an external file.
     create_table_mssql_from_external_file = SQLExecuteQueryOperator(
         task_id="create_table_from_external_file",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql="create_table.sql",
         dag=dag,
     )
     populate_user_table = SQLExecuteQueryOperator(
         task_id="populate_user_table",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql=r"""
                 INSERT INTO Users (username, description)
                 VALUES ( 'Danny', 'Musician');
@@ -74,17 +74,17 @@ with DAG(
     )
     get_all_countries = SQLExecuteQueryOperator(
         task_id="get_all_countries",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql=r"""SELECT * FROM Country;""",
     )
     get_all_description = SQLExecuteQueryOperator(
         task_id="get_all_description",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql=r"""SELECT description FROM Users;""",
     )
     get_countries_from_continent = SQLExecuteQueryOperator(
         task_id="get_countries_from_continent",
-        conn_id="airflow_mssql",
+        conn_id="sql_id",
         sql=r"""SELECT * FROM Country where {{ params.column }}='{{ params.value }}';""",
         params={"column": "CONVERT(VARCHAR, continent)", "value": "Asia"},
     )
