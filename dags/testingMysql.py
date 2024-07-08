925e3964ad4a33ae638f2e827ea8ed6f38447bd5
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-07-08 09:57:35 +0100

    Update testingMysql.py

--- dags/testingMysql.py
+++ dags/testingMysql.py
@@ -1,43 +1,97 @@
-from datetime import datetime
+import pytest
+
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
-default_args = {
-    'owner': 'Fouda',
-    'depends_on_past': False,
-    'start_date': datetime(2025, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0
-}
-# Define the DAG
-dag = DAG(
-    'MysqlServerConnection',
-    default_args=default_args,
-    description='Testing SQL server connection',
-    schedule_interval=None
-)
+try:
+    from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+    from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
+except ImportError:
+    pytest.skip("MSSQL provider not available", allow_module_level=True)
+
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "example_mssql"
 
 
-task1 = EmptyOperator(task_id='task1')
+with DAG(
+    DAG_ID,
+    schedule="@daily",
+    start_date=datetime(2021, 10, 1),
+    tags=["example"],
+    catchup=False,
+) as dag:
 
-# Task to create the table
-create_pet_table = SQLExecuteQueryOperator(
-        task_id="create_pet_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS pet (
-            pet_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            pet_type VARCHAR NOT NULL,
-            birth_date DATE NOT NULL,
-            OWNER VARCHAR NOT NULL);
-          """,
-    
-    dag=dag,
-)
+    # Example of creating a task to create a table in MsSql
 
+    create_table_mssql_task = SQLExecuteQueryOperator(
+        task_id="create_country_table",
+        conn_id="airflow_mssql",
+        sql=r"""
+        CREATE TABLE Country (
+            country_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+            name TEXT,
+            continent TEXT
+        );
+        """,
+        dag=dag,
+    )
 
+    @dag.task(task_id="insert_mssql_task")
+    def insert_mssql_hook():
+        mssql_hook = MsSqlHook(mssql_conn_id="airflow_mssql", schema="airflow")
 
-task1 >> create_pet_table
-
+        rows = [
+            ("India", "Asia"),
+            ("Germany", "Europe"),
+            ("Argentina", "South America"),
+            ("Ghana", "Africa"),
+            ("Japan", "Asia"),
+            ("Namibia", "Africa"),
+        ]
+        target_fields = ["name", "continent"]
+        mssql_hook.insert_rows(table="Country", rows=rows, target_fields=target_fields)
+    # Example of creating a task that calls an sql command from an external file.
+    create_table_mssql_from_external_file = SQLExecuteQueryOperator(
+        task_id="create_table_from_external_file",
+        conn_id="airflow_mssql",
+        sql="create_table.sql",
+        dag=dag,
+    )
+    populate_user_table = SQLExecuteQueryOperator(
+        task_id="populate_user_table",
+        conn_id="airflow_mssql",
+        sql=r"""
+                INSERT INTO Users (username, description)
+                VALUES ( 'Danny', 'Musician');
+                INSERT INTO Users (username, description)
+                VALUES ( 'Simone', 'Chef');
+                INSERT INTO Users (username, description)
+                VALUES ( 'Lily', 'Florist');
+                INSERT INTO Users (username, description)
+                VALUES ( 'Tim', 'Pet shop owner');
+                """,
+    )
+    get_all_countries = SQLExecuteQueryOperator(
+        task_id="get_all_countries",
+        conn_id="airflow_mssql",
+        sql=r"""SELECT * FROM Country;""",
+    )
+    get_all_description = SQLExecuteQueryOperator(
+        task_id="get_all_description",
+        conn_id="airflow_mssql",
+        sql=r"""SELECT description FROM Users;""",
+    )
+    get_countries_from_continent = SQLExecuteQueryOperator(
+        task_id="get_countries_from_continent",
+        conn_id="airflow_mssql",
+        sql=r"""SELECT * FROM Country where {{ params.column }}='{{ params.value }}';""",
+        params={"column": "CONVERT(VARCHAR, continent)", "value": "Asia"},
+    )
+    (
+        create_table_mssql_task
+        >> insert_mssql_hook()
+        >> create_table_mssql_from_external_file
+        >> populate_user_table
+        >> get_all_countries
+        >> get_all_description
+        >> get_countries_from_continent
+    )
