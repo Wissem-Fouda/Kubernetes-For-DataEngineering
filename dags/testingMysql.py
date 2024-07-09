2f27690599251fa97fbf2de927358f6f4cce3421
Wissem Fouda <106931464+Wissem-Fouda@users.noreply.github.com>
2024-07-09 11:22:25 +0100

    Update testingMysql.py

--- dags/testingMysql.py
+++ dags/testingMysql.py
@@ -1,99 +1,23 @@
-import os
-from datetime import datetime
-import pytest
-
 from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from datetime import datetime
 
-try:
-    from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-    from airflow.providers.microsoft.mssql.hooks.mssql import MsSqlHook
-except ImportError:
-    pytest.skip("MSSQL provider not available", allow_module_level=True)
+default_args = {
+    'owner': 'Fouda',
+    'start_date': datetime(2023, 1, 1),
+    'catchup': False,
+}
 
-ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "example_mssql"
+with DAG('docker_mysql_dag',
+         default_args=default_args,
+         schedule_interval='@daily',
+         ) as dag:
 
-
-with DAG(
-    DAG_ID,
-    schedule="@daily",
-    start_date=datetime(2021, 10, 1),
-    tags=["example"],
-    catchup=False,
-) as dag:
-
-    # Example of creating a task to create a table in MsSql
-
-    create_table_mssql_task = SQLExecuteQueryOperator(
-        task_id="create_country_table",
-        conn_id="sql_id",
-        sql=r"""
-        CREATE TABLE Country (
-            country_id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-            name TEXT,
-            continent TEXT
-        );
-        """,
-        dag=dag,
-    )
-
-    @dag.task(task_id="insert_mssql_task")
-    def insert_mssql_hook():
-        mssql_hook = MsSqlHook(mssql_conn_id="airflow_mssql", schema="airflow")
-
-        rows = [
-            ("India", "Asia"),
-            ("Germany", "Europe"),
-            ("Argentina", "South America"),
-            ("Ghana", "Africa"),
-            ("Japan", "Asia"),
-            ("Namibia", "Africa"),
-        ]
-        target_fields = ["name", "continent"]
-        mssql_hook.insert_rows(table="Country", rows=rows, target_fields=target_fields)
-    # Example of creating a task that calls an sql command from an external file.
-    create_table_mssql_from_external_file = SQLExecuteQueryOperator(
-        task_id="create_table_from_external_file",
-        conn_id="sql_id",
-        sql="create_table.sql",
-        dag=dag,
-    )
-    populate_user_table = SQLExecuteQueryOperator(
-        task_id="populate_user_table",
-        conn_id="sql_id",
-        sql=r"""
-                INSERT INTO Users (username, description)
-                VALUES ( 'Danny', 'Musician');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Simone', 'Chef');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Lily', 'Florist');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Tim', 'Pet shop owner');
-                """,
-    )
-    get_all_countries = SQLExecuteQueryOperator(
-        task_id="get_all_countries",
-        conn_id="sql_id",
-        sql=r"""SELECT * FROM Country;""",
-    )
-    get_all_description = SQLExecuteQueryOperator(
-        task_id="get_all_description",
-        conn_id="sql_id",
-        sql=r"""SELECT description FROM Users;""",
-    )
-    get_countries_from_continent = SQLExecuteQueryOperator(
-        task_id="get_countries_from_continent",
-        conn_id="sql_id",
-        sql=r"""SELECT * FROM Country where {{ params.column }}='{{ params.value }}';""",
-        params={"column": "CONVERT(VARCHAR, continent)", "value": "Asia"},
-    )
-    (
-        create_table_mssql_task
-        >> insert_mssql_hook()
-        >> create_table_mssql_from_external_file
-        >> populate_user_table
-        >> get_all_countries
-        >> get_all_description
-        >> get_countries_from_continent
+    run_sql_insert_container = KubernetesPodOperator(
+        namespace='airflow',
+        image='foudazdocker/mysql:1.0',  
+        cmds=["python", "./SQL_insert.py"],  
+        name="run-sql-insert-container",
+        task_id="run_sql_insert_container_task",
+        get_logs=True
     )
